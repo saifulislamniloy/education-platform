c8ae0dcbdb628060c3220c3e8b9fb38745098cd3
saifulislamniloy <software.engineer.niloy@gmail.com>
2025-08-09 00:38:13 +0600

    setup project structure

---
+++ back_end/src/test/java/io/github/saifulislamniloy/education_platform/EducationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.saifulislamniloy.education_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
